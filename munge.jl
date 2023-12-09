0833856cff2723c3b28eb05ba348adaf7cc576fb
Tom Gillam <tpgillam@googlemail.com>
2023-12-09 13:08:26 +0000

    Some plotting

--- munge.jl
+++ munge.jl
@@ -4,7 +4,6 @@ using DataFrames
 using Missings
 using Query
 using TimeZones
-using PlotlyJS
 using Plots
 using StatsBase
 using StatsPlots
@@ -27,25 +26,53 @@ function get_path(day::Date)::String
     return joinpath("daily-text", Dates.format(day, "yyyy_mm_dd"))
 end
 
-function get_frame(date_range)::DataFrame
+function _get_time(date::Date, raw_time, occurrence::Integer)
+    return DateTime(ZonedDateTime(date + raw_time, tz"Europe/London", occurrence), UTC)
+end
+
+function _to_datetimes(date::Date, raw_times)
+    times = DateTime[]
+    sizehint!(times, length(raw_times))
+    for raw_time in raw_times
+        time = _get_time(date, raw_time, 1)
+        time = if !isempty(times) && last(times) >= time
+            _get_time(date, raw_time, 2)
+        else
+            time
+        end
+        push!(times, time)
+    end
+    return times
+end
+
+function get_frame(date::Date)
+    path = get_path(date)
+    !isfile(path) && return missing
+    frame = CSV.File(path; header=COLUMNS, comment="#") |> DataFrame
+
+    # Use first occurrence... seems like there's something iffy in the raw data around
+    # DST changes in any case!
+    insertcols!(frame, 1, :DateTime => _to_datetimes(date, frame[!, :Time]))
+
+    if nrow(frame) != length(unique(frame[!, :DateTime]))
+        error("Bad times found on $date")
+    end
+
+    # Fix rainfall.
+    if !(eltype(frame[!, "Rain / mm"]) <: Number)
+        frame[!, "Rain / mm"] = parse.(Float64, replace(frame[!, "Rain / mm"], "*" => "0"))
+    end
+
+    # frame[!, :Date] .= date
+    return frame
+end
+
+function get_frame(date_range::AbstractVector{Date})
     frame = nothing
     for date in date_range
-        path = get_path(date)
-        this_frame = CSV.File(path; header=COLUMNS, comment="#") |> DataFrame
-
-        # Use first occurrence... seems like there's something iffy in the raw data around
-        # DST changes in any case!
-        insertcols!(
-            this_frame,
-            1,
-            :DateTime => ZonedDateTime.(date .+ this_frame[!, :Time], tz"Europe/London", 1)
-        )
-        # this_frame[!, :Date] .= date
-        if isnothing(frame)
-            frame = this_frame
-        else
-            frame = vcat(frame, this_frame)
-        end
+        this_frame = get_frame(date)
+        ismissing(this_frame) && continue
+        frame = isnothing(frame) ? this_frame : vcat(frame, this_frame)
     end
 
     # Check that we haven't been bitten by DST ambiguities.
@@ -61,7 +88,7 @@ end
 
 """Make a data frame summarising the weather on each day."""
 function summarise(frame::AbstractDataFrame)::DataFrame
-    frame[!, :Date] = frame[!, :DateTime].|> zdt -> Date(zdt.utc_datetime)
+    frame[!, :Date] = frame[!, :DateTime] .|> zdt -> Date(zdt.utc_datetime)
     gdf = groupby(frame, :Date)
 
     summaries = []
@@ -90,20 +117,40 @@ function summarise(frame::AbstractDataFrame)::DataFrame
 end
 
 # The date range to process. End points are inclusive.
-t_start = Date(2017, 1, 1)
-t_end = Date(2021, 6, 19)
+# t_start = Date(2017, 1, 1)
+# t_start = Date(1995, 6, 30)  # This is the first day with data
+t_start = Date(2000, 1, 1)
+t_end = Date(2023, 5, 6)
 
 # Get one dataframe for the range specified above. This will put the data from all files
 # into a single dataframe.
 frame = get_frame(t_start:Day(1):t_end)
 
+function _get_delta_rainfall(rain_mm::AbstractVector{<:AbstractFloat})
+    result = Float64[]
+    sizehint!(result, length(rain_mm))
+    # Placeholder that indicates that we haven't had a previous value.
+    prev_x = Inf
+    for x in rain_mm
+        if (isempty(result) || x < prev_x)
+            push!(result, x)
+        else
+            push!(result, x - prev_x)
+        end
+        prev_x = x
+    end
+    return result
+end
+
+delta_rain = _get_delta_rainfall(frame[!, "Rain / mm"])
+
 # Plot a long timeseries of all data we've processed above.
 plotlyjs()  # Allow interactivity.
 Plots.plot(
     frame[!, :DateTime],
-    # frame[!, "Rain / mm"];
+    cumsum(delta_rain);
     # frame[!, "Sun / hours"];
-    frame[!, "Humidity / %"];
+    # frame[!, "Humidity / %"];
     linetype=:steppost,
     ticks=:native
 )
@@ -117,7 +164,7 @@ summary_frame = summarise(frame)
     [:humidity_min :humidity_mean :humidity_max];
     linetype=:steppost,
     ticks=:native,
-    ylabel="Humidity / %",
+    ylabel="Humidity / %"
 )
 Plots.savefig("humidity.png")
 
@@ -126,7 +173,7 @@ Plots.savefig("humidity.png")
     [:temp_min :temp_mean :temp_max];
     linetype=:steppost,
     ticks=:native,
-    ylabel="Temperature / °C",
+    ylabel="Temperature / °C"
 )
 Plots.savefig("temperature.png")
 
@@ -135,7 +182,7 @@ Plots.savefig("temperature.png")
     [:pressure_min :pressure_mean :pressure_max];
     linetype=:steppost,
     ticks=:native,
-    ylabel="Pressure / mBar",
+    ylabel="Pressure / mBar"
 )
 Plots.savefig("pressure.png")
 
